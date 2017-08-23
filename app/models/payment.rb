92414ba7155c4fa1f5b3341b243b6c18bbfc6716
Andrew Katz <andrewkatz00@gmail.com>
2017-08-23 09:20:05 -0700

    Fix moving dates - update next pay date if due on changes

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,6 +3,8 @@ class Payment < ActiveRecord::Base
 
   validates :name, :next_pay_date, :due_on, presence: true
 
+  before_save :adjust_next_pay_date, if: :due_on_changed?
+
   def pay
     self.last_paid_date = Time.zone.now
     update_next_pay_date
@@ -15,7 +17,7 @@ class Payment < ActiveRecord::Base
 
   def friendly_days_left
     days = days_left
-    return 'Today' if days == 0
+    return 'Today' if days.zero?
 
     days_str = [days, 'day'.pluralize(days)].join(' ')
     in_past? ? [days_str, 'ago'].join(' ') : days_str
@@ -25,23 +27,11 @@ class Payment < ActiveRecord::Base
     if next_pay_date
       self.next_pay_date += 1.month
     else
-      if Time.zone.now.day > due_on
-        self.next_pay_date = (Time.zone.now + 1.month).beginning_of_month
-      else
-        self.next_pay_date ||= Time.zone.now
-      end
+      set_next_pay_date
     end
 
-    while self.next_pay_date.day < due_on
-      break if (self.next_pay_date + 1.day).month != self.next_pay_date.month
-      self.next_pay_date += 1.day
-    end
-
-    while !allow_weekends && (next_pay_date.saturday? || next_pay_date.sunday?)
-      self.next_pay_date -= 1.day
-    end
-
-    update_next_pay_date if next_pay_date < Time.zone.now
+    adjust_next_pay_date
+    update_next_pay_date if in_past?
   end
 
   def days_left
@@ -50,18 +40,36 @@ class Payment < ActiveRecord::Base
 
     return 0 if next_pay_date == today
 
-    if next_pay_date > today
-      difference = next_pay_date - today
-    else
-      difference = today - next_pay_date
-    end
+    difference = if next_pay_date > today
+                   next_pay_date - today
+                 else
+                   today - next_pay_date
+                 end
 
     difference.to_i
   end
 
   private
 
+  def set_next_pay_date
+    now = Time.zone.now
+    self.next_pay_date = now
+    self.next_pay_date += 1.month if now.day > due_on
+  end
+
+  def adjust_next_pay_date
+    last_day_of_month = self.next_pay_date.end_of_month.day
+    day = [due_on, last_day_of_month].min
+    self.next_pay_date = Time.zone.local(next_pay_date.year, next_pay_date.month, day)
+
+    self.next_pay_date -= 1.day while !allow_weekends && weekend?
+  end
+
   def in_past?
     self.next_pay_date.to_date < Time.zone.now.to_date
+  end
+
+  def weekend?
+    next_pay_date.saturday? || next_pay_date.sunday?
   end
 end
