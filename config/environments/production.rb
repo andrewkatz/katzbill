e803abc2233ef081b37f711c37ad3ff726001423
Andrew Katz <andrewkatz00@gmail.com>
2017-08-31 22:35:05 -0700

    Set default production from email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'billsandstuff.com' }
+  config.action_mailer.default_options = { from: 'no-reply@billsandstuff.com' }
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
